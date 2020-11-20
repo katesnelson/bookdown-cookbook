01babef71390e13915a3e6fbb86019d7cbab1e88
Katherine S Nelson <ksnelson@ksu.edu>
2020-11-20 10:47:34 -0600

    add pics

--- _deploy.sh
+++ _deploy.sh
@@ -5,7 +5,7 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
+git config --global user.email "ksnelson@ksu.edu"
 git config --global user.name "katesnelson"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
